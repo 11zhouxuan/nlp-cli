903d20e7b2265112770c6982118fee533709cf5b
11zhouxuan <15110180025@fudan.edu.cn>
2019-11-14 20:59:50 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time : 2019/11/13 19:50
-# @Author : by 周旋
+# @Author : by zhou xuan
 # @File : setup.py
 # @Software: PyCharm
 # plt.switch_backend('Qt5Agg')
@@ -10,7 +10,7 @@ from setuptools import setup,find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 setup(
-    name = 'nlpycli',
+    name = 'nlpcli',
     version = '1.0',
     author = 'Zhou Xuan',
     author_email = '15110180025@fudan.edu.cn',
